c2887d9f6412bfe813e1108dcf3c7d42946b9789
willemsleegers <w.sleegers@me.com>
2023-07-04 15:06:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         headers: [
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://unrivaled-cranachan-f0332f.netlify.app",
+          },
         ],
       },
     ]
